e2713157c31dce2c167287d2e40397a4e72b92a8
Furkan Demirel <furkandemireleng@gmail.com>
2024-03-18 23:24:38 +0300

    Read repository interface created

--- src/Core/ECommerce.Application/Interfaces/Repositories/IWriteRepository.cs
+++ src/Core/ECommerce.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -1,6 +1,14 @@
+using ECommerce.Domain.Common;
+
 namespace ECommerce.Application.Interfaces.Repositories;
 
-public interface IWriteRepository
+public interface IWriteRepository<T> where T : class, IBaseEntity, new()
 {
-    
+    Task<int> AddAsync(T entity);
+    Task<int> AddRangeAsync(IList<T> entity);
+
+    Task<T> UpdateAsync(T entity);
+
+    Task HardDeleteAsync(T entity);
+    Task SoftDeleteAsync(T entity);
 }
