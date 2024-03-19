9c22422b1ba27f705585e673847a5f40748ffbec
Furkan Demirel <furkandemireleng@gmail.com>
2024-03-19 12:31:41 +0300

    Add mapper class library into core

---
+++ src/Core/ECommerce.Application/Interfaces/AutoMapper/IAutoMapper.cs
@@ -0,0 +1,12 @@
+namespace ECommerce.Application.Interfaces.AutoMapper;
+
+public interface IAutoMapper
+{
+    TDestination Map<TDestination, TSource>(TSource source, string? ignore = null);
+
+    IList<TDestination> Map<TDestination, TSource>(IList<TSource> source, string? ignore = null);
+
+    TDestination Map<TDestination, TSource>(object source, string? ignore = null);
+
+    IList<TDestination> Map<TDestination, TSource>(IList<object> source, string? ignore = null);
+}
