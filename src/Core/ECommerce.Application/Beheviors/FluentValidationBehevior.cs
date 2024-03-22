0d254c1a5e1cfa72dc95238ff2d7959ac6eecee3
Furkan Demirel <furkandemireleng@gmail.com>
2024-03-23 01:24:20 +0300

    fluent validation added

---
+++ src/Core/ECommerce.Application/Beheviors/FluentValidationBehevior.cs
@@ -0,0 +1,6 @@
+namespace ECommerce.Application.Beheviors;
+
+public class FluentValidationBehevior
+{
+    
+}
---
+++ src/Core/ECommerce.Application/Features/Products/Command/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,6 @@
+namespace ECommerce.Application.Features.Products.Command.CreateProduct;
+
+public class CreateProductCommandValidator
+{
+    
+}
